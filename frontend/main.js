aa249b972e7b365d15618a2b094713e731e9094b
EFSIGOD <fbranca@pioix.edu.ar>
2024-07-10 08:51:50 -0300

    Linkeo de log y reg

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,6 @@
 let bancoDepalabras = []
 let users = []
+let games = []
 console.log(bancoDepalabras)
 let gridX = 5 
 let gridY = 6
@@ -17,13 +18,13 @@ arrancarJuego()
 function encontrarUserPorID(id) {
     for (let i = 0; i < users.length; i++) {
         if (users[i].id === id) {
-            return i; // Devuelve la posición del cliente en el vector
+            return i; // Devuelve la posición del user en el vector
         }
     }
-    return -1; // Si no se encuentra el cliente, devuelve -1
+    return -1; // Si no se encuentra el user, devuelve -1
 }
 
-//ejercicio 18, función login FUNCIONA
+//función login FUNCIONA
 function login(username, password){
     for (let i = 0; i < users.length; i++) {
         if (users[i].Username == username && users[i].Password==password) {
@@ -50,7 +51,7 @@ function linkLogin(){
 }
 
 //ejercicio 19, función registro FUNCIONA
-function register(dni, password, nameUser, surname, ingresosAnuales){
+function register(usuario, mail, contra, nombre, apellido){
     if(dni<1000000){
         return -1
     }else if(password.length<5){
@@ -59,11 +60,11 @@ function register(dni, password, nameUser, surname, ingresosAnuales){
         return -3
     }else if(surname.length<3){
         return -4
-    }else if(ingresosAnuales<100000){ //política bancaria-> mínimo 100000 anual
+    }else if(mail.length<6){ 
         return -5
     }else{
-        clients.push(new Client(dni, password, nameUser, surname, ingresosAnuales))
-        return idClient-1
+        userId=postUser(usuario, mail, contra, nombre, apellido)
+        return 
     }
 }
 
@@ -74,7 +75,7 @@ function linkRegister(){
     mail=getMailUser()
     nameUser=getNameUser()
     surname=getSurnameUser()
-    userId=register(username, password, mail, nameUser, surname)
+    userId=register(username, mail, password, nameUser, surname)
     if (clientId>0){
         userId=encontrarClientePorID(userId)
         screenGame()
@@ -88,16 +89,44 @@ function linkRegister(){
     }else if(clientId==-4){
         window.alert("Apellido muy corto")
     }else if(clientId==-5){
-        window.alert("solo admitimos clientes con más de 100000 pesos de ganancias anuales POBRE")
+        window.alert("Mail inválido")
     }
 }
 
-//ejercicio 20, función de logout FUNCIONA
+//función de logout FUNCIONA
 function logout(){
     userId = -1
     posUser= -1
     screenLogin()
     window.alert("Ha cerrado su cuenta con éxito")
+}
+
+async function postUser(usuario, mail, contra, nombre, apellido){
+    if(confirm("tas seguro?")){
+        const user={
+            Username:usuario,
+            Email_Address:mail,
+            Password:contra,
+            Name:nombre,
+            Surname:apellido
+        }
+        try{
+            const response = await fetch('http://localhost:3000/insertUser', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+            });
+            if (!response.ok) {
+                throw new Error('Failed to insert data');
+            }else{
+            window.alert("se ha creado el usuario")
+            }
+        }catch (error) {
+            console.error('Error inserting data:', error);
+        }
+    }
 }
 
 //------------------------------------------------------- Funciones de juego ----------------------------------------------------------------------
